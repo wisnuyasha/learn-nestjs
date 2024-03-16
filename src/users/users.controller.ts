f8100a8868487250b2a1047cf69dff913529f1be
unknown <wisnuyeahfaizal@gmail.com>
2024-03-16 13:43:06 +0700

    feat: add users crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // get all users
+  @Get()
+  findAll(): [] {
+    return [];
+  }
+
+  // get all users/admins w/ query
+  @Get('admins')
+  findAllAdmins(@Query('role') role?: 'one' | 'two' | 'three') {
+    return [];
+  }
+
+  // get by users by id
+  @Get()
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // create user
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  // update user by id
+  @Patch()
+  update(@Param('id') id: string, @Body() updatedUser: {}) {
+    return { id, updatedUser };
+  }
+
+  // delete user by id
+  @Delete()
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
